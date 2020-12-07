2b3e98cf7ad0af7150ce8fd7833392b7bd9b0056
L16H7 <linmyatko@hotmail.com>
2020-12-07 12:07:16 +0630

    refactoring

--- lambdas/database_functions.py
+++ lambdas/database_functions.py
@@ -1,8 +1,8 @@
 import sys
 import logging
 import pymysql
-#rds settings
-rds_host  = 'awba.c7xhqvq8qokj.ap-southeast-1.rds.amazonaws.com'
+# rds settings
+rds_host = 'awba.c7xhqvq8qokj.ap-southeast-1.rds.amazonaws.com'
 name = 'root'
 password = 'toor2020'
 db_name = 'awba'
@@ -11,17 +11,21 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 try:
-    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    conn = pymysql.connect(rds_host, user=name,
+                           passwd=password, db=db_name, connect_timeout=5)
     print('connected to database')
     print(conn)
 except pymysql.MySQLError as e:
-    logger.error('ERROR: Unexpected error: Could not connect to MySQL instance.')
+    logger.error(
+        'ERROR: Unexpected error: Could not connect to MySQL instance.')
     logger.error(e)
     sys.exit()
- 
+
+
 def save(image_key, label, confidence):
     with conn.cursor() as cur:
-        cur.execute("INSERT INTO image_detections (image_key, label, confidence) VALUES (%s, %s, %s)", (image_key, label, confidence))
+        cur.execute("INSERT INTO image_detections (image_key, label, confidence) VALUES (%s, %s, %s)",
+                    (image_key, label, confidence))
         conn.commit()
     conn.commit()
 
--- lambdas/lambda_function.py
+++ lambdas/lambda_function.py
@@ -1,45 +1,17 @@
 import json
+import rekognitions
 import database_functions
 
+
 def lambda_handler(event, context):
-    key = event['Records'][0]['s3']['object']['key']
-    
-    # detect_labels(key, bucket)
-    detect_labels(key, 'awba-objects')
-    
+    image_key = event['Records'][0]['s3']['object']['key']
+
+    detections = rekognitions.detect_image_labels(image_key, 'awba-objects')
+
+    for label in detections:
+        database_functions.save(image_key, label['Name'], label['Confidence'])
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
-
-import boto3
-
-def detect_labels(photo, bucket):
-
-    client=boto3.client('rekognition')
-
-    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
-        MaxLabels=10)
-
-    print('Detected labels for ' + photo) 
-    print()   
-    for label in response['Labels']:
-        print ("Label: " + label['Name'])
-        print ("Confidence: " + str(label['Confidence']))
-        database_functions.save(photo, label['Name'], label['Confidence'])
-        # print ("Instances:")
-        # for instance in label['Instances']:
-        #     print ("  Bounding box")
-        #     print ("    Top: " + str(instance['BoundingBox']['Top']))
-        #     print ("    Left: " + str(instance['BoundingBox']['Left']))
-        #     print ("    Width: " +  str(instance['BoundingBox']['Width']))
-        #     print ("    Height: " +  str(instance['BoundingBox']['Height']))
-        #     print ("  Confidence: " + str(instance['Confidence']))
-        #     print()
-
-        print ("Parents:")
-        for parent in label['Parents']:
-            print ("   " + parent['Name'])
-        print ("----------")
-        print ()
-    return len(response['Labels'])
