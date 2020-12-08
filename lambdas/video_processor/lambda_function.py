15d7950eea6cd81ad80592ac01d616b67fe13409
L16H7 <linmyatko@hotmail.com>
2020-12-08 14:38:06 +0630

    video processor

--- lambdas/video_processor/lambda_function.py
+++ lambdas/video_processor/lambda_function.py
@@ -5,19 +5,22 @@ rek = boto3.client('rekognition')
 roleArn = 'arn:aws:iam::044650912587:role/Rekognition'
 snsTopicArn = 'arn:aws:sns:ap-southeast-1:044650912587:AmazonRekognitionSNS'
 
-def start_label_detection():
+
+def start_label_detection(video_key):
     print(roleArn)
     print(snsTopicArn)
-    response=rek.start_label_detection(Video={'S3Object': {'Bucket': 'awba-videos', 'Name': 'sample.mp4'}},
-        NotificationChannel={'RoleArn': roleArn, 'SNSTopicArn': snsTopicArn})
+    response = rek.start_label_detection(Video={'S3Object': {'Bucket': 'awba-videos', 'Name': video_key}},
+                                         NotificationChannel={'RoleArn': roleArn, 'SNSTopicArn': snsTopicArn})
 
     print(response)
-    startJobId=response['JobId']
+    startJobId = response['JobId']
     print('Start Job Id: ' + startJobId)
-    
+
+
 def lambda_handler(event, context):
-    start_label_detection()
-    
+    video_key = event['Records'][0]['s3']['object']['key']
+    start_label_detection(video_key)
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
