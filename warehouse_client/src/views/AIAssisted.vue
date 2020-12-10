39a20048f9d408992d5065b20969e43882b3c532
L16H7 <linmyatko@hotmail.com>
2020-12-10 11:55:11 +0630

    api server init

--- warehouse_client/src/views/AIAssisted.vue
+++ warehouse_client/src/views/AIAssisted.vue
@@ -1,7 +1,31 @@
 <template>
-  <v-container> Here is an A.I </v-container>
+  <v-container>
+    <v-chip class="ma-2" color="primary" outlined pill>
+      Image and Video processing proof-of-concept
+      <v-icon right> mdi-account-outline </v-icon>
+    </v-chip>
+    <v-row>
+      <v-col>
+        <v-file-input outlined prepend-icon="mdi-camera" v-model="image" />
+      </v-col>
+      <v-col>
+        <v-btn
+          large
+          outlined
+          color="primary"
+          @click="upload"
+        >
+          Upload
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    image: null,
+  }),
+};
 </script>
