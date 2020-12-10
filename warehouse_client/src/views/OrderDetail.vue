607196d3eb0b9c16d28574ac86eb9c258ae22cf3
L16H7 <linmyatko@hotmail.com>
2020-12-10 19:56:50 +0630

    order detail table

--- warehouse_client/src/views/OrderDetail.vue
+++ warehouse_client/src/views/OrderDetail.vue
@@ -1,39 +1,55 @@
 <template>
   <v-container>
-    Order Detail
-    <v-form class="mt-5">
-      <v-text-field
-        readonly
-        v-model="order.name"
-        outlined
-        label="Name"
-      />
-      <v-text-field
-        readonly
-        v-model="order.phone"
-        outlined
-        label="Phone"
-      />
-      <v-text-field
-        readonly
-        v-model="order.email"
-        outlined
-        label="Email"
-      />
-      <v-textarea
-        readonly
-        v-model="order.delivery_address"
-        outlined
-        label="Delivery Address"
-      />
-      <v-textarea
-        readonly
-        v-model="order.billing_address"
-        outlined
-        label="Billing Address"
-      />
-    </v-form>
-    {{ order.cart }}
+    <v-row>
+      <v-col cols="6">
+        <v-chip class="ma-2" color="primary" outlined pill>
+          Order Items
+          <v-icon right> mdi-cart-outline </v-icon>
+        </v-chip>
+        <v-data-table
+          :headers="headers"
+          :items="JSON.parse(order.cart)"
+        />
+        <v-chip class="ma-2" color="primary" outlined pill>
+          Total: {{ total }} MMK
+          <v-icon right> mdi-dollar-outline </v-icon>
+        </v-chip>
+      </v-col>
+      <v-col>
+        <v-form class="mt-5">
+          <v-text-field
+            readonly
+            v-model="order.name"
+            outlined
+            label="Name"
+          />
+          <v-text-field
+            readonly
+            v-model="order.phone"
+            outlined
+            label="Phone"
+          />
+          <v-text-field
+            readonly
+            v-model="order.email"
+            outlined
+            label="Email"
+          />
+          <v-textarea
+            readonly
+            v-model="order.delivery_address"
+            outlined
+            label="Delivery Address"
+          />
+          <v-textarea
+            readonly
+            v-model="order.billing_address"
+            outlined
+            label="Billing Address"
+          />
+        </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -45,5 +61,28 @@ export default {
       default: () => {},
     },
   },
+
+  computed: {
+    total() {
+      return JSON.parse(this.order.cart).reduce((acc, cur) => acc + cur.amount, 0);
+    },
+  },
+
+  data: () => ({
+    headers: [
+      {
+        text: 'Item',
+        value: 'name',
+      },
+      {
+        text: 'QTY',
+        value: 'qty',
+      },
+      {
+        text: 'Amount',
+        value: 'amount',
+      },
+    ],
+  }),
 };
 </script>
