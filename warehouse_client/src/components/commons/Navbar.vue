cf33eb4dc2d08bc590d0a20e8ff83841d5437564
L16H7 <linmyatko@hotmail.com>
2020-12-10 11:20:28 +0630

    ai page

---
+++ warehouse_client/src/components/commons/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+    <v-btn @click="goto('/ai')" text>
+      <span class="mr-2">AI assisted</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    goto(route) {
+      this.$router.push(route);
+    },
+  },
+};
+</script>
---
+++ warehouse_client/src/views/AIAssisted.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container> Here is an A.I </v-container>
+</template>
+
+<script>
+export default {};
+</script>
