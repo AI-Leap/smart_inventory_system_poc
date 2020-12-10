8ed41d0416943b4c20f0f8ede8b6b020600d3bd9
L16H7 <linmyatko@hotmail.com>
2020-12-10 13:08:40 +0630

    image upload api call

--- warehouse_client/src/views/AIAssisted.vue
+++ warehouse_client/src/views/AIAssisted.vue
@@ -9,23 +9,31 @@
         <v-file-input outlined prepend-icon="mdi-camera" v-model="image" />
       </v-col>
       <v-col>
-        <v-btn
-          large
-          outlined
-          color="primary"
-          @click="upload"
-        >
-          Upload
-        </v-btn>
+        <v-btn large outlined color="primary" @click="upload"> Upload </v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     image: null,
   }),
+
+  methods: {
+    async upload() {
+      const formData = new FormData();
+      formData.append('image', this.image);
+      const ret = await axios.post('http://localhost:4000/api/ai', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(ret);
+    },
+  },
 };
 </script>
