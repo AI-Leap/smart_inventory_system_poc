f00c7962653e6f9ec18c9771db901992e9f762d8
L16H7 <linmyatko@hotmail.com>
2020-12-10 21:01:15 +0630

    sortable

--- warehouse_client/src/main.js
+++ warehouse_client/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import Sortable from 'vue-sortable';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+Vue.use(Sortable);
 
 new Vue({
   router,
