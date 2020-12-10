5d5bd35b7a4418984dc7bd28016de89b39046366
L16H7 <linmyatko@hotmail.com>
2020-12-10 18:08:09 +0630

    store

---
+++ ecommerce_client/src/components/orders/ItemCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card elevation="2" outlined shaped>
+    <v-card-title> {{ item.name }} </v-card-title>
+    <v-card-text>
+      <v-img contain :src="item.url" max-height="200" />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat officiis
+      obcaecati consequatur, molestiae autem nostrum voluptatibus commodi
+      cumque! Assumenda, iusto praesentium officia quisquam illum
+    </v-card-text>
+    <v-card-actions>
+      <v-row>
+        <v-col cols="6">
+          <v-text-field type="number" outlined small placeholder="QTY" v-model="qty" />
+        </v-col>
+        <v-col>
+          <v-btn color="success" outlined large @click="addToCart">
+            Add To Cart
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+
+  data: () => ({
+    qty: 0,
+  }),
+
+  methods: {
+    addToCart() {
+      this.$store.dispatch('addToCart', {
+        id: this.item.id,
+        qty: this.qty,
+      });
+    },
+  },
+};
+</script>
--- ecommerce_client/src/views/Order.vue
+++ ecommerce_client/src/views/Order.vue
@@ -2,45 +2,18 @@
   <v-container>
     <v-row>
       <v-col cols="3" v-for="(item, i) in inventory" :key="i">
-        <v-card :key="i" elevation="2" outlined shaped>
-          <v-card-title> {{ item.name }} </v-card-title>
-          <v-card-text>
-            <v-img
-              contain
-              :src="item.url"
-              max-height="200"
-            />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
-            officiis obcaecati consequatur, molestiae autem nostrum voluptatibus
-            commodi cumque! Assumenda, iusto praesentium officia quisquam illum
-          </v-card-text>
-          <v-card-actions>
-            <v-row>
-              <v-col cols="6">
-                <v-text-field
-                  type="number"
-                  outlined
-                  small
-                  placeholder="QTY"
-                />
-              </v-col>
-              <v-col>
-                <v-btn color="success" outlined large @click="addToCart(i)"> Add To Cart </v-btn>
-              </v-col>
-            </v-row>
-          </v-card-actions>
-        </v-card>
+        <ItemCard :item="item" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import ItemCard from '@/components/orders/ItemCard.vue';
+
 export default {
-  methods: {
-    addToCart(itemId) {
-      this.$store.dispatch('addToCart', itemId);
-    },
+  components: {
+    ItemCard,
   },
 
   computed: {
