8f88019340db1b5f025636c872a67a27d9ceea56
L16H7 <linmyatko@hotmail.com>
2020-12-10 16:07:53 +0630

    items

---
+++ ecommerce_client/src/views/Order.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="3" v-for="i in 9" :key="i">
+        <v-card :key="i" elevation="2" outlined shaped>
+          <v-card-title> Product 1 </v-card-title>
+          <v-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            officiis obcaecati consequatur, molestiae autem nostrum voluptatibus
+            commodi cumque! Assumenda, iusto praesentium officia quisquam illum
+            magnam fuga laboriosam nobis at voluptatum!
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary"> Add To Cart </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
