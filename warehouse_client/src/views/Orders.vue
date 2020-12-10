8839b5af3302bf3af80b47a68e2bd2edf0c043b3
L16H7 <linmyatko@hotmail.com>
2020-12-10 19:28:50 +0630

    order table

--- warehouse_client/src/views/Orders.vue
+++ warehouse_client/src/views/Orders.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-container>{{ orders }}</v-container>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="orders"
+    />
+  </v-container>
 </template>
 
 <script>
@@ -8,12 +13,39 @@ import axios from 'axios';
 export default {
   data: () => ({
     orders: [],
+    headers: [
+      {
+        text: 'Order ID',
+        value: 'id',
+      },
+      {
+        text: 'Name',
+        value: 'name',
+      },
+      {
+        text: 'Phone',
+        value: 'phone',
+      },
+      {
+        text: 'Email',
+        value: 'email',
+      },
+      {
+        text: 'Delivery Address',
+        value: 'delivery_address',
+      },
+      {
+        text: 'Billing Address',
+        value: 'billing_address',
+      },
+    ],
   }),
 
   methods: {
     async getOrders() {
       const url = `${process.env.VUE_APP_SERVER_URL}/orders`;
-      this.orders = await axios.get(url);
+      const ret = await axios.get(url);
+      this.orders = ret.data;
     },
   },
 
