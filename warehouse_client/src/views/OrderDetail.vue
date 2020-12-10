97d24b07f4010707def84a0db4d589d0ccf1ee84
L16H7 <linmyatko@hotmail.com>
2020-12-10 19:51:56 +0630

    order detail

--- warehouse_client/src/views/OrderDetail.vue
+++ warehouse_client/src/views/OrderDetail.vue
@@ -1,6 +1,39 @@
 <template>
   <v-container>
-    {{ order }}
+    Order Detail
+    <v-form class="mt-5">
+      <v-text-field
+        readonly
+        v-model="order.name"
+        outlined
+        label="Name"
+      />
+      <v-text-field
+        readonly
+        v-model="order.phone"
+        outlined
+        label="Phone"
+      />
+      <v-text-field
+        readonly
+        v-model="order.email"
+        outlined
+        label="Email"
+      />
+      <v-textarea
+        readonly
+        v-model="order.delivery_address"
+        outlined
+        label="Delivery Address"
+      />
+      <v-textarea
+        readonly
+        v-model="order.billing_address"
+        outlined
+        label="Billing Address"
+      />
+    </v-form>
+    {{ order.cart }}
   </v-container>
 </template>
 
