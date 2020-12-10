99bcf72eaed688c8dafd0c99afc917e17c745f13
L16H7 <linmyatko@hotmail.com>
2020-12-10 19:13:43 +0630

    order create complete

--- warehouse_api_server/controllers/order.controller.js
+++ warehouse_api_server/controllers/order.controller.js
@@ -1,6 +1,13 @@
+const connection = require('../database/connection');
+
 const controller = {
   createOrder: async (req, res) => {
     console.log(req.body);
+    const { name, phone, email, deliveryAddress, billingAddress, cart } = req.body;
+    const query = `INSERT INTO orders (name, phone, email, delivery_address, billing_address, cart)
+      VALUES ('${name}', '${phone}', '${email}', '${deliveryAddress}', '${billingAddress}', '${JSON.stringify(cart)}')`
+    
+    const ret = await connection.query(query);
     return res.status(201).send();
   },
 };
